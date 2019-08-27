dff737222e9a54ade0fb6b242da7b90063ab5820
vagrant <vagrant@homestead>
2019-08-27 07:10:53 +0000

    收获地址列表

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,8 @@
 
 Route::get('/', 'PagesController@root')->name('root');
 Auth::routes(['verify' => true]);
-
+Route::group(['middleware' => ['auth', 'verified']], function() {
+    Route::get('user_addresses', 'UserAddressesController@index')->name('user_addresses.index');
+});
 
 Route::get('/home', 'HomeController@index')->name('home');
